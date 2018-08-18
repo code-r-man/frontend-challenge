92696b9914c6060c13bbfd028bc89b3b1defccd3
Nikola Jovanovic <ing.nikola.jovanovic@gmail.com>
2018-08-18 13:36:22 +0200

    Re-style checkout bubble.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 'use strict';$(document).ready(function(){var products=[];// Add the product to the total products array
 function addProduct(_id){products.push(_id);updateCart()}// Update total number of items in cart
-function updateCart(){var totalNumber=products.length;console.log(totalNumber);if(totalNumber>0){$('.js-cart-bubble').css({display:'flex'}).text(totalNumber)}}// Add the itme when user clicks on the 'add cart button'
+function updateCart(){var totalNumber=products.length;console.log(totalNumber);if(totalNumber>0){$('.js-cart-bubble').addClass('in').text(totalNumber)}}// Add the itme when user clicks on the 'add cart button'
 $('.js-item-add').click(function(event){// Get the item id and push it
 addProduct($(this).parents('.card').data('item'))})});
